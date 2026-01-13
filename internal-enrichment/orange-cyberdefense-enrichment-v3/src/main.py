f226effef662d5c83293e21c4496056cebf7bf9c
Arthur CAUCHY <arthur.cauchy@orangecyberdefense.com>
2026-01-13 15:14:47 +0100

    fix: score and tags

--- internal-enrichment/orange-cyberdefense-enrichment-v3/src/main.py
+++ internal-enrichment/orange-cyberdefense-enrichment-v3/src/main.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import os
+import re
 import sys
 
 import stix2
@@ -220,15 +221,16 @@ class OrangeCyberdefenseEnrichment:
             stix_obj["labels"] = []
 
     def _process_object_scores(self, stix_obj):
-        if "x_datalake_score" in stix_obj:
+        if "x_datalake_score" in stix_obj and self.ocd_enrich_add_score:
             scores = list(stix_obj["x_datalake_score"].values())
             if len(scores) > 0:
                 stix_obj["x_opencti_score"] = max(scores)
-        threat_scores = stix_obj.get("x_datalake_score", {})
-        for threat_type, score in threat_scores.items():
-            ranged_score = utils.get_ranged_score(score)
-            new_label = f"dtl_{threat_type}_{ranged_score}"
-            stix_obj["labels"].append(new_label)
+        if self.ocd_enrich_add_scores_as_labels:
+            threat_scores = stix_obj.get("x_datalake_score", {})
+            for threat_type, score in threat_scores.items():
+                ranged_score = utils.get_ranged_score(score)
+                new_label = f"dtl_{threat_type}_{ranged_score}"
+                stix_obj["labels"].append(new_label)
 
     def _process_object_extrefs(self, stix_obj):
         if "external_references" in stix_obj:
@@ -363,7 +365,7 @@ class OrangeCyberdefenseEnrichment:
 
         return markdown_str
 
-    def _generate_observable_note(self, indicator_object, stix_entity):
+    def _generate_observable_note(self, indicator_object, observable_object):
         creation_date = indicator_object.get("created", {})
         technical_md = self._generate_indicator_markdown(indicator_object)
         note_stix = stix2.Note(
@@ -374,7 +376,7 @@ class OrangeCyberdefenseEnrichment:
             modified=indicator_object["modified"],
             created_by_ref=indicator_object.get("created_by_ref", None),
             object_marking_refs=[self.marking["standard_id"]],
-            object_refs=[stix_entity["id"], indicator_object["id"]],
+            object_refs=[observable_object["id"], indicator_object["id"]],
         )
         return note_stix
 
@@ -383,7 +385,7 @@ class OrangeCyberdefenseEnrichment:
         value = observable["observable_value"]
 
         stix_objects = data["stix_objects"]
-        stix_entity = data["stix_entity"]
+        observable_object = data["stix_entity"]
 
         tlp = "TLP:CLEAR"
         for marking_definition in observable.get("objectMarking", []):
@@ -410,7 +412,6 @@ class OrangeCyberdefenseEnrichment:
         if "threat_found" in data:
             self.helper.log_info(f"No threat found for {value}")
             return
-
         self.helper.log_info(f"Match found for {value}")
 
         indicator_object = {}
@@ -423,36 +424,38 @@ class OrangeCyberdefenseEnrichment:
                 continue
             related_objects.append(processed_object)
 
-        labels = []
-        max_score = -1
-
-        threat_scores = indicator_object.get("x_datalake_score", {})
-        for threat_type, score in threat_scores.items():
-            ranged_score = utils.get_ranged_score(score)
-            new_label = f"dtl_{threat_type}_{ranged_score}"
-            labels.append(new_label)
-            max_score = max(max_score, score)
-
-        if self.ocd_enrich_add_score and max_score != -1:
+        if "x_opencti_score" in indicator_object:
             OpenCTIStix2.put_attribute_in_extension(
-                stix_entity, STIX_EXT_OCTI_SCO, "score", max_score
+                observable_object,
+                STIX_EXT_OCTI_SCO,
+                "score",
+                indicator_object.get("x_opencti_score", None),
             )
 
+        # Split indicator labels between score related and standard
+        score_labels = []
+        standard_labels = []
+        for label in indicator_object.get("labels", []):
+            if re.fullmatch(r"dtl_[a-z]+_\d+", label):
+                score_labels.append(label)
+            else:
+                standard_labels.append(label)
+
         if self.ocd_enrich_add_scores_as_labels:
-            for label in labels:
+            for score_label in score_labels:
                 OpenCTIStix2.put_attribute_in_extension(
-                    stix_entity,
+                    observable_object,
                     STIX_EXT_OCTI_SCO,
                     "labels",
-                    label,
+                    score_label,
                     True,
                 )
 
         if self.ocd_enrich_add_tags_as_labels:
-            labels = utils.curate_labels(indicator_object.get("labels", []))
+            labels = utils.curate_labels(standard_labels)
             for label in labels:
                 OpenCTIStix2.put_attribute_in_extension(
-                    stix_entity,
+                    observable_object,
                     STIX_EXT_OCTI_SCO,
                     "labels",
                     label,
@@ -478,7 +481,7 @@ class OrangeCyberdefenseEnrichment:
                             ),
                         )
                         self.helper.api.stix_cyber_observable.add_external_reference(
-                            id=stix_entity["id"],
+                            id=observable_object["id"],
                             external_reference_id=ext_ref["id"],
                         )
                     except Exception as e:
@@ -489,7 +492,7 @@ class OrangeCyberdefenseEnrichment:
         if self.ocd_enrich_add_summary:
             try:
                 note_stix = self._generate_observable_note(
-                    indicator_object, stix_entity
+                    indicator_object, observable_object
                 )
                 stix_objects.append(json.loads(note_stix.serialize()))
             except Exception as e:
@@ -503,11 +506,11 @@ class OrangeCyberdefenseEnrichment:
                 id=StixCoreRelationship.generate_id(
                     relationship_type="based-on",
                     source_ref=indicator_object["id"],
-                    target_ref=stix_entity["id"],
+                    target_ref=observable_object["id"],
                 ),
                 relationship_type="based-on",
                 source_ref=indicator_object["id"],
-                target_ref=stix_entity["id"],
+                target_ref=observable_object["id"],
                 created_by_ref=(
                     self.identity["standard_id"]
                     if self.ocd_enrich_add_createdby
